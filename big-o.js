caf7741cfa7f2ec84a12d8630b397a03928e7f7b
CodeyWorley <codeyworley@gmail.com>
2019-01-15 09:40:17 -0700

    finished big-o

---
+++ big-o.js
@@ -0,0 +1,38 @@
+// Even or Odd 
+// Constant time O(1)
+// doesnt matter what the input is it will just run once
+
+// Are you here?
+// Polynomial time O(n^2)
+// Nested forloop -> requires two levels of looping.
+
+// Doubler
+// Linear time O(n)
+// input is directly related to the output, therefore linear.
+
+// Naive Search
+// Linear time O(n)
+// Different run times based on where the search item is in the 
+// array. But it just runs through the loop once.
+
+// Creating Pairs
+// Exponential time O(2^n)
+// As the length of the array grows the outputs for each index is multiplied
+// by the length of the array
+
+// Computing Fibonaccis
+// Linear time O(n)
+// the size of the output array is directly related to the input.
+
+// An Efficient Search
+// Logarithmic time O(log(n))
+// The length of the array has a halfed impact on the run time. So run times 
+// grow disproportionately slower as the data grows.
+
+// Random Element
+// Constant time O(1)
+// function only runs once
+
+// Is It Prime?
+// Linear time O(n)
+// the function is only run once to find a single input to create and output.
